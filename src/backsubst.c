4a559536655a7fe45fcfa9da2163592d5aa6bd58
ItsDinosaur <maciejk.rutkowski@outlook.com>
2023-12-18 10:26:47 +0100

    Dodane podstawienie wsteczne

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,15 +5,23 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-			 * Tutaj należy umieścić właściwą implemntację.
-				 */
-
-			/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-	int i;
-	for (i =0; i < x->r; i++) {
-		x->data[i][0] = b->data[i][0];
+	//sprawdzenie czy wymiary macierzy sa zgodne
+	if(b->r != x->r || b->r != mat->r || x->r != mat->r || mat->c != mat->r){
+		return 2;
+	}
+	//zaczynam od ostatniego wiersza i sie cofam
+	for(int w = (b->r) - 1; w>=0; w--){
+		double pom=0;
+		//obliczam pom, gdzie ax + pom = b
+		for(int k = w+1; k<(b->r); k++){
+			pom+= mat->data[w][k] * x->data[k][0];
+		}
+		//sprawdzam czy a nie jest 0
+		if((mat->data[w][w]) < 1e-6){
+			return 1;
+		}
+		//podstawiam pod x, x=(b-pom) / a
+		x->data[w][0] = (b->data[w][0] - pom) / (mat->data[w][w]);
 	}
 
 	return 0;
